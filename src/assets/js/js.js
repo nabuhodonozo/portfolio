a0927b4c90913c6843272cde7144468cab176e9f
Tomasz Czarnecki <tomasz@czarnecki.uk>
2018-07-08 23:35:38 +0200

    Deleted scrollbar, added anchors

--- src/assets/js/js.js
+++ src/assets/js/js.js
@@ -6,13 +6,24 @@ $(function () {
 
     $('.colapse').on('click', function (e) {
         $(this).nextAll().slideToggle();
-        if($(this).hasClass('fa-caret-down')){
+        if ($(this).hasClass('fa-caret-down')) {
             $(this).removeClass('fa-caret-down');
             $(this).addClass('fa-caret-up');
-        }else{
+        } else {
             $(this).removeClass('fa-caret-up');
             $(this).addClass('fa-caret-down');
         }
         e.preventDefault();
     });
+
+$('a').click(function(){		
+		var hashindex = $(this).attr("href").indexOf('#');
+		var hreflen = $(this).attr("href").length;
+		var anchortag = $(this).attr("href").substr(hashindex, hreflen);
+    $('html, body').animate({
+        scrollTop: $( anchortag ).offset().top
+    }, 700);
+    return false;
+	});
+
 });
